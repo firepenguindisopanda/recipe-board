2ab050b9dfd9fb4931a61cf0671eff1bffef29ff
Nicholas <nicosmith.smith3@gmail.com>
2021-04-12 20:43:22 +0000

    update user.py in controllers folder

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -2,6 +2,15 @@ from flask import redirect, render_template, request, session, url_for
 
 from App.models import ( User )
 
+'''
 def create_user(firstname, lastname, uwi_id, email, gender, dob):
     # newuser = use()
-    return 'new user'
+    newuser = User(firstname=firstname, lastname=lastname)
+    db.session.add(newuser)
+    db.session.commit()
+'''
+def create_user(firstname, lastname):
+    # newuser = use()
+    newuser = User(firstname=firstname, lastname=lastname)
+    db.session.add(newuser)
+    db.session.commit()
