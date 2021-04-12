7ea498509141be71970731962a261e187c6d99e6
Nicholas <nicosmith.smith3@gmail.com>
2021-04-12 22:43:46 +0000

    updated index and user.py

--- App/views/user.py
+++ App/views/user.py
@@ -3,7 +3,8 @@ from flask import Blueprint, redirect, render_template, request, jsonify, send_f
 user_views = Blueprint('user_views', __name__, template_folder='../templates')
 
 from App.models import User
-from App.controllers import ( get_users, get_users_json, create_user )
+#from App.controllers import ( get_users, get_users_json, create_user )
+from App.controllers import ( create_user )
 
 @user_views.route('/users', methods=['GET'])
 def get_user_page():
