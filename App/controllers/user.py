a951a34f53b3cb5b8fd82f6a79c68fe2135002e2
Nicholas <nicosmith.smith3@gmail.com>
2021-04-13 03:15:46 +0000

    Implemented two func for user.py in Controllers.

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -14,3 +14,12 @@ def create_user(firstname, lastname):
     newuser = User(firstname=firstname, lastname=lastname)
     db.session.add(newuser)
     db.session.commit()
+
+def get_users_json():
+    users = User.query.all()
+    if not users:
+        return jsonify([])
+    json = [user.toDict() for user in users]
+    return json
+def get_users():
+    return User.query.all()
