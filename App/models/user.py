452c1e895f0bd38c580323750e57c439da7a4514
Nicholas <nicosmith.smith3@gmail.com>
2021-04-20 19:17:10 +0000

    added more entities to User model

--- App/models/user.py
+++ App/models/user.py
@@ -7,10 +7,27 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name =  db.Column(db.String, nullable=False)
     last_name =  db.Column(db.String, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
     def toDict(self):
         return{
             'id': self.id,
             'first_name': self.first_name,
-            'last_name': self.last_name
+            'last_name': self.last_name,
+            'username': self.username,
+            'email': self.email,
+            'password': self.password
         }
+    
+    def set_password(self, password):
+        """Create hashed password."""
+        self.password = generate_password_hash(password, method='sha256')
+
+    def check_password(self, password):
+        """Check hashed password."""
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
