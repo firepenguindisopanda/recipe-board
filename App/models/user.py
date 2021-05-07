0ec34c0c18ea810ad5f6fefa75d16c5b4d1a808e
Nicholas <nicosmith.smith3@gmail.com>
2021-05-07 03:06:40 +0000

    removed a duplicate import statement

--- App/models/user.py
+++ App/models/user.py
@@ -3,7 +3,6 @@ from flask import Flask, request, render_template, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import json
-from flask_login import UserMixin
 db=SQLAlchemy()
 
 class User(db.Model, UserMixin):
