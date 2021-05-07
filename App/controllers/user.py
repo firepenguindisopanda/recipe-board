1caf64f2551833f47a8960436daea3f8b53010f6
Nicholas <nicosmith.smith3@gmail.com>
2021-05-07 03:05:56 +0000

    removed commented code

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,14 +1,8 @@
 from flask import redirect, render_template, request, session, url_for
+from flask_login import LoginManager, current_user, login_user, login_required, logout_user, UserMixin
 
 from App.models import ( User, db )
 
-'''
-def create_user(firstname, lastname, uwi_id, email, gender, dob):
-    # newuser = use()
-    newuser = User(firstname=firstname, lastname=lastname)
-    db.session.add(newuser)
-    db.session.commit()
-'''
 def create_user(first_name, last_name, username, email, password):
     # newuser = use()
     newuser = User(first_name=first_name, last_name=last_name, username=username, email=email)
